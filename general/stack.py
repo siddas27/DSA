db7f61e575e0f5387e529d6e7dc867e20a02612a
Siddharth Das <siddharthdas8@gmail.com>
2021-12-19 19:26:04 -0700

    q

--- general/stack.py
+++ general/stack.py
@@ -1,9 +1,20 @@
 from abc import ABC, abstractmethod
+from sys import maxsize
 
+
+class StackNode():
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
 class Stack(ABC):
+
     @abstractmethod
-    def push(self):
+    def create(self):
+        pass
+
+    @abstractmethod
+    def push(self, x):
         pass
 
     @abstractmethod
@@ -19,4 +30,52 @@ class Stack(ABC):
         pass
 
 
-class StackUsing
+class StackUsingList(Stack):
+    def create(self):
+        self.stack = []
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def pop(self):
+        if self.is_empty():
+            return "Stack Underflow"
+
+        return self.stack.pop()
+
+    def top(self):
+        if self.is_empty():
+            return "Stack Underflow"
+
+        return self.stack[len(self.stack)-1]
+
+    def push(self, x):
+        if len(self.stack) == maxsize:
+            return "Stack Overflow"
+
+        self.stack.append(x)
+
+class StackUsingLinkedList(Stack):
+    def create(self):
+        self.root = None
+
+    def push(self, x):
+        new_node = StackNode(x)
+        new_node.next = self.root
+        self.root = new_node
+
+    def is_empty(self):
+        return self.root == None
+
+    def pop(self):
+        if self.root == None:
+            return "Stack Underflow"
+        temp = self.root
+        self.root = self.root.next
+        return temp.data
+
+    def top(self):
+        if self.root == None:
+            return "Stack Underflow"
+        return self.root.data
+
